75e92c59c5d86f78292b8276fac5c0292845eda9
chu1070y <chu1070y@naver.com>
2018-09-18 19:33:16 +0900

    fdsa

--- src/domain/BoardVO.java
+++ src/domain/BoardVO.java
@@ -1,5 +1,14 @@
 package domain;
 
+import java.util.Date;
+
+import lombok.Data;
+
+@Data
 public class BoardVO {
+
+	private int mno, bno;
+	private String name, id, title, cnt, answer, addfile;
+	private Date regdate, updatedate;
 
 }

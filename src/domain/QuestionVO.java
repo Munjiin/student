a69e2a48b0dd2cc948681b1bb71b637c723fbfa9
chu1070y <chu1070y@naver.com>
2018-09-18 19:36:18 +0900

    dx

---
+++ src/domain/QuestionVO.java
@@ -0,0 +1,13 @@
+package domain;
+
+import java.util.Date;
+
+import lombok.Data;
+
+@Data
+public class QuestionVO {
+	private int qno, limittime, mno, reply;
+	private String question, cmt, name, id;
+	private Date regdate, replydate;
+	
+}

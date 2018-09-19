9eaf18f0d1f2fcbe17267b7d2a2f754813565c7d
jiin <answldls9401@hanmail.net>
2018-09-19 13:22:15 +0900

    bb

--- src/dao/BoardDAO.java
+++ src/dao/BoardDAO.java
@@ -1,5 +1,57 @@
 package dao;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import domain.BoardVO;
+import domain.PageDTO;
+
 public class BoardDAO {
+    private static  final String LIST = "select *\n" +
+            "from (select\n" +
+            "             /*+ INDEX_DESC (tbl_board pk_board) */\n" +
+            "          ROWNUM rn, bno, title, content, WRITER, REGDATE, UPDATEDATE, viewcnt \n" +
+            "      from TBL_BOARD\n" +
+            "      where bno > 0\n" +
+            "        and ROWNUM <= (? * ?))\n" +
+            "where rn > (?-1) * ?";
+	
+	public List<BoardVO> getList(PageDTO pageDTO)throws Exception{
+        List<BoardVO> list = new ArrayList<>();
+
+        new QueryExecutor() {
+            @Override
+            public void doJob() throws Exception {
+                stmt = con.prepareStatement(LIST);
+                int i = 1;
+                stmt.setInt(i++,pageDTO.getPage());
+                stmt.setInt(i++,pageDTO.getSize());
+                stmt.setInt(i++,pageDTO.getPage());
+                stmt.setInt(i++,pageDTO.getSize());
+                rs = stmt.executeQuery();
+
+                while(rs.next()){
+                    BoardVO vo = new BoardVO();
+                    int idx = 2;
+          
+                    vo.setMno(rs.getInt(idx++));
+                    vo.setBno(rs.getInt(idx++));
+                    vo.setName(rs.getString(idx++));
+                    vo.setId(rs.getString(idx++));
+                    vo.setTitle(rs.getString(idx++));
+                    vo.setCnt(rs.getString(idx++));
+                    vo.setAnswer(rs.getString(idx++));
+                    vo.setAddfile(rs.getString(idx++));
+                    vo.setRegdate(rs.getDate(idx++));
+                    vo.setUpdatedate(rs.getDate(idx++));
+                  
+                    list.add(vo);
+                }
+
+            }
+        }.executeAll();
+
+        return list;
+    }
 
 }
--- src/web/BoardController.java
+++ src/web/BoardController.java
@@ -1,8 +1,51 @@
 package web;
 
-import dao.BoardDAO;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-public class BoardController {
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import domain.BoardVO;
+
+import dao.BoardDAO;
+import domain.PageDTO;
+import domain.PageMaker;
+import web.util.Converter;
+
+@WebServlet(urlPatterns="/user/*")
+public class BoardController extends AbstractController {
 
 	BoardDAO dao = new BoardDAO();
+	
+	
+	 public String listGET(HttpServletRequest req, HttpServletResponse resp) throws Exception{
+	        System.out.println("listGET...........................");
+	    
+			
+			
+
+	        PageDTO dto = PageDTO.of()
+	                .setPage(Converter.getInt(req.getParameter("page"),1))
+	                .setSize(Converter.getInt(req.getParameter("size"),10));
+
+	        int total = 320;
+	        PageMaker pageMaker = new PageMaker(total,dto);
+
+	        req.setAttribute("pageMaker",pageMaker);
+	        req.setAttribute("list",dao.getList(dto));
+
+	        return "list";
+	    }
+
+	 
+	 
+	 
+	@Override
+	public String getBasic() {
+		
+		return "/board/";
+	}
 }

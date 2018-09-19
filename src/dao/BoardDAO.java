e8f4a47b2de9ff8c2734bb98861de82cc7fbce4c
jiin <answldls9401@hanmail.net>
2018-09-19 17:08:32 +0900

    ㅜㅜ

--- src/dao/BoardDAO.java
+++ src/dao/BoardDAO.java
@@ -1,57 +1,47 @@
 package dao;
 
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
 import domain.BoardVO;
 import domain.PageDTO;
+import domain.QuestionVO;
 
 public class BoardDAO {
-    private static  final String LIST = "select *\n" +
-            "from (select\n" +
-            "             /*+ INDEX_DESC (tbl_board pk_board) */\n" +
-            "          ROWNUM rn, bno, title, content, WRITER, REGDATE, UPDATEDATE, viewcnt \n" +
-            "      from TBL_BOARD\n" +
-            "      where bno > 0\n" +
-            "        and ROWNUM <= (? * ?))\n" +
-            "where rn > (?-1) * ?";
 	
-	public List<BoardVO> getList(PageDTO pageDTO)throws Exception{
-        List<BoardVO> list = new ArrayList<>();
+	//QuestionDAO
+		private String preFix =  "mapper.questiondMapper";
+		
+		//MybatisLoader
+		static SqlSessionFactory sqlSessionFactory;
 
-        new QueryExecutor() {
-            @Override
-            public void doJob() throws Exception {
-                stmt = con.prepareStatement(LIST);
-                int i = 1;
-                stmt.setInt(i++,pageDTO.getPage());
-                stmt.setInt(i++,pageDTO.getSize());
-                stmt.setInt(i++,pageDTO.getPage());
-                stmt.setInt(i++,pageDTO.getSize());
-                rs = stmt.executeQuery();
-
-                while(rs.next()){
-                    BoardVO vo = new BoardVO();
-                    int idx = 2;
-          
-                    vo.setMno(rs.getInt(idx++));
-                    vo.setBno(rs.getInt(idx++));
-                    vo.setName(rs.getString(idx++));
-                    vo.setId(rs.getString(idx++));
-                    vo.setTitle(rs.getString(idx++));
-                    vo.setCnt(rs.getString(idx++));
-                    vo.setAnswer(rs.getString(idx++));
-                    vo.setAddfile(rs.getString(idx++));
-                    vo.setRegdate(rs.getDate(idx++));
-                    vo.setUpdatedate(rs.getDate(idx++));
-                  
-                    list.add(vo);
-                }
-
-            }
-        }.executeAll();
-
-        return list;
-    }
+		static{
+			try {
+				String resource = "mybatis-config.xml";
+		         InputStream inputStream = Resources.getResourceAsStream(resource);
+		          sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+			}catch(Exception e) {
+			e.printStackTrace();
+		}
+		}
+		
+		
+				
+		
+		public List<BoardVO> getList(PageDTO dto) {
+			
+			try(SqlSession session = sqlSessionFactory.openSession(true)){
+				return session.selectList(preFix + ".selectPage", dto.getPage());
+			}catch(Exception e) {
+				e.printStackTrace();
+		}
+			return null;
+		}
 
 }
--- src/web/BoardController.java
+++ src/web/BoardController.java
@@ -1,5 +1,6 @@
 package web;
 
+import java.io.InputStream;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -8,28 +9,85 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
 import domain.BoardVO;
 
 import dao.BoardDAO;
+import dao.QuestionDAO;
 import domain.PageDTO;
 import domain.PageMaker;
 import web.util.Converter;
 
-@WebServlet(urlPatterns="/user/*")
+@WebServlet(urlPatterns="/user/board/*")
 public class BoardController extends AbstractController {
+	
+	
+	
+	
+	QuestionDAO dao = new QuestionDAO();
+
+	//list
+	 public String listGET(HttpServletRequest req, HttpServletResponse resp) throws Exception {
+	
+
+	        PageDTO dto = PageDTO.of()
+	                .setPage(Converter.getInt(req.getParameter("page"), 1))
+	                .setSize(Converter.getInt(req.getParameter("size"), 10));
+
+	        int total = 320;
+	        PageMaker pageMaker = new PageMaker(total, dto);
+
+	        req.setAttribute("pageMaker", pageMaker);
+	        req.setAttribute("selectPage", dao.getList(dto));
+
+	        return "list";
+	    }
+	 public String getBasic() {
+	        return "/user/board/";
+	    }
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	/*
 
 	BoardDAO dao = new BoardDAO();
+	
+
 	
 	
 	 public String listGET(HttpServletRequest req, HttpServletResponse resp) throws Exception{
 	        System.out.println("listGET...........................");
-	    
-			
-			
 
 	        PageDTO dto = PageDTO.of()
 	                .setPage(Converter.getInt(req.getParameter("page"),1))
 	                .setSize(Converter.getInt(req.getParameter("size"),10));
+	        
+	        
 
 	        int total = 320;
 	        PageMaker pageMaker = new PageMaker(total,dto);
@@ -47,5 +105,5 @@ public class BoardController extends AbstractController {
 	public String getBasic() {
 		
 		return "/board/";
-	}
+	}*/
 }
